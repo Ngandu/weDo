ed9265f349a96b29d41d2d2b87a7bb5bcc8fdd00
IQ3OY97LwdTaBaDqYw2PKSzQN1IKGnn126GOB9b+hBU= <pngandu@yahoo.com>
2022-08-23 10:50:46 +0200

    add to array
    Add todo to array

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const todoModal = document.querySelector("#modal");
 const todoAddButton = document.querySelector(".addButton");
 const modalCover = document.querySelector(".cover");
+const addTodo = document.querySelector(".addTodo");
+
+// Data
+let todos = [];
+
+// Funstions
+
+const hideForm = ()=>{
+  todoModal.classList.remove("active");
+  modalCover.classList.remove("active");
+
+}
 
 todoAddButton.addEventListener("click", () => {
   todoModal.classList.add("active");
@@ -8,6 +20,28 @@ todoAddButton.addEventListener("click", () => {
 });
 
 modalCover.addEventListener("click", () => {
-  todoModal.classList.remove("active");
-  modalCover.classList.remove("active");
+  hideForm();
 });
+
+
+// Add Todo
+
+addTodo.addEventListener("click", ()=>{
+  event.preventDefault();
+  let form = document.querySelector('#addForm');
+  let title = document.querySelector('.newTitle').value;
+  let date = document.querySelector('.newDate').value;
+  if(title.length > 3 && date.length > 5){
+    // create an element
+    let newTd = {"title": title, "date": date, "isComplete": false};
+    todos.push(newTd);
+  }
+
+  form.reset();
+
+  console.log('todos: ',todos);
+
+  // Hide form
+  hideForm();
+
+})
